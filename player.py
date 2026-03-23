c70bc34c6f4350f0b56daea52db74286c391d64e
KrisPKreme <S3935358@student.rmit.edu.au>
2026-03-23 18:44:44 +1100

    basic simulation of player 1 movement around board

--- player.py
+++ player.py
@@ -1,7 +1,11 @@
 class Player:
-    def __init__(self, name, balance, position):
+    def __init__(self, name, balance, curr_space):
         self.name = name
         self.balance = balance
-        self.position = position
+        self.curr_space = 0
 
-        
+    def move(self, steps, board_size):
+        old_position = self.curr_space
+        self.curr_space = (self.curr_space + steps) % board_size
+
+        return self.curr_space
--- program.py
+++ program.py
@@ -1,8 +1,23 @@
 import json
+from player import Player
 
 with open('board.json', 'r') as file:
-    data = json.load(file)
+    board = json.load(file)
+
+with open('rolls_1.json', 'r') as file:
+    rolls_1 = json.load(file)
 
 
+Player1 = Player("Peter", 16, 0)
+Player2 = Player("Billy", 16, 0)
+Player3 = Player("Charlotte", 16, 0)
+Player4 = Player("Sweedal", 16, 0)
 
-print(json.dumps(data, indent=4))
+space = Player1.move(rolls_1[0], 9)
+print(board[space]['name'])
+
+space = Player1.move(rolls_1[1], 9)
+print(board[space]['name'])
+
+
+# print(json.dumps(board, indent=4))
