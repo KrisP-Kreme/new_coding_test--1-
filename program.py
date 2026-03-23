34b4c685f3ab325a99bdfc2a99496a2bb6db7410
KrisPKreme <S3935358@student.rmit.edu.au>
2026-03-23 21:14:43 +1100

    simulating full game

--- program.py
+++ program.py
@@ -1,6 +1,10 @@
 import json
 from player import Player
 
+board_size = 9
+starting_balance = 16
+starting_position = 0
+
 with open('board.json', 'r') as file:
     board = json.load(file)
 
@@ -8,16 +12,31 @@ with open('rolls_1.json', 'r') as file:
     rolls_1 = json.load(file)
 
 
-Player1 = Player("Peter", 16, 0)
-Player2 = Player("Billy", 16, 0)
-Player3 = Player("Charlotte", 16, 0)
-Player4 = Player("Sweedal", 16, 0)
+Player1 = Player("Peter", starting_balance, starting_position)
+Player2 = Player("Billy", starting_balance, starting_position)
+Player3 = Player("Charlotte", starting_balance, starting_position)
+Player4 = Player("Sweedal", starting_balance, starting_position)
 
-space = Player1.move(rolls_1[0], 9)
-print(board[space]['name'])
+curr_player = Player1
 
-space = Player1.move(rolls_1[1], 9)
-print(board[space]['name'])
+for roll in rolls_1:
+    space = curr_player.move(rolls_1[roll], board_size)
+    print(f"{curr_player.name} landed on {board[space]['name']}")
+
+    if curr_player == Player1:
+        curr_player = Player2
+    elif curr_player == Player2:
+        curr_player = Player3
+    elif curr_player == Player3:
+        curr_player = Player4
+    elif curr_player == Player4:
+        curr_player = Player1
+
+# space = Player1.move(rolls_1[0], 9)
+# print(board[space]['name'])
+
+# space = Player1.move(rolls_1[1], 9)
+# print(board[space]['name'])
 
 
 # print(json.dumps(board, indent=4))
