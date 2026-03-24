8daeb02a40840f198dd5b807a2300c1ef1616c2e
KrisPKreme <S3935358@student.rmit.edu.au>
2026-03-24 21:09:21 +1100

    separated landing on properties and handling of end of game into separate functions to clean up simulation loop

--- program.py
+++ program.py
@@ -20,6 +20,9 @@ with open('board.json', 'r') as file:
 with open('rolls_1.json', 'r') as file:
     rolls_1 = json.load(file)
 
+with open('rolls_2.json', 'r') as file:
+    rolls_2 = json.load(file)
+
 # group spaces by colour in dictionary
 colour_groups = {}
 for space in board:
@@ -37,53 +40,60 @@ Player4 = Player("Sweedal", starting_balance, starting_position)
 
 players = [Player1, Player2, Player3, Player4]
 
+# handle landing on property spaces
+def handle_land_on_property(player, space):
+    if board[space].owner is None:
+        # player lands on unowned property and must buy it
+        curr_player.balance -= board[space].price
+        board[space].owner = curr_player
+        print(f"{curr_player.name} bought {board[space].name} for {board[space].price} and has a balance of {curr_player.balance}")
+    else:
+        # player lands on owned property and must pay rent to owner
+        if board[space].owner != curr_player:
+            # rent is doubled if all properties of the same colour are owned by the same player
+            if all(s.owner == board[space].owner for s in colour_groups[board[space].colour]):
+                rent = board[space].price * 2
+            else:
+                rent = board[space].price
+            curr_player.balance -= rent
+            for player in players:
+                if player == board[space].owner and curr_player.balance >= 0:
+                    player.balance += rent
+            print(f"{curr_player.name} landed on {board[space].name}, paid {rent} rent to {board[space].owner.name} and has a balance of {curr_player.balance}")
+        # player lands on their own property and does nothing
+        elif board[space].owner == curr_player:
+            print(f"{curr_player.name} landed on their own property {board[space].name} and has a balance of {curr_player.balance}")
 
+# handle end game when a player goes bankrupt
+def handle_end_game():
+    max_balance = players[0].balance
+    winner = players[0]
+    for player in players:
+        if player.balance > max_balance:
+            max_balance = player.balance
+            winner = player
+    print("Final Outcome: ")
+    print(f"{curr_player.name} has gone bankrupt. {winner.name} wins.")
+    for player in players: 
+        if player.balance < 0:
+            print(f"{player.name} is bankrupt and finished on {board[player.curr_space].name}.")           
+        else:
+            print(f"{player.name} has a balance of {player.balance} and finished on {board[player.curr_space].name}.")
+    exit()
+
+
+# loop for simulating game 
 curr_index = 0
-
 for roll in rolls_1:
     curr_player = players[curr_index]
     space = curr_player.move(roll, board_size)
 
-    # print(f"{curr_player.name} rolled {roll} and landed on {board[space].name} and has a balance of {curr_player.balance}")
-
+    # check if a player has landed on a property space and handle accordingly
     if board[space].type == "property":
-        if board[space].owner is None:
-            # player lands on unowned property and must buy it
-            curr_player.balance -= board[space].price
-            board[space].owner = curr_player
-            print(f"{curr_player.name} bought {board[space].name} for {board[space].price} and has a balance of {curr_player.balance}")
-        else:
-            # player lands on owned property and must pay rent to owner
-            if board[space].owner != curr_player:
-                # rent is doubled if all properties of the same colour are owned by the same player
-                if all(s.owner == board[space].owner for s in colour_groups[board[space].colour]):
-                    rent = board[space].price * 2
-                else:
-                    rent = board[space].price
-                curr_player.balance -= rent
-                for player in players:
-                    if player == board[space].owner and curr_player.balance >= 0:
-                        player.balance += rent
-                print(f"{curr_player.name} paid {rent} rent to {board[space].owner.name} and has a balance of {curr_player.balance}")
-            # player lands on their own property and does nothing
-            elif board[space].owner == curr_player:
-                print(f"{curr_player.name} landed on their own property {board[space].name} and has a balance of {curr_player.balance}")
+        handle_land_on_property(curr_player, space)
 
     # check if player has gone bankrupt and end game if so
     if curr_player.balance < 0:
-        max_balance = players[0].balance
-        winner = players[0]
-        for player in players:
-            if player.balance > max_balance:
-                max_balance = player.balance
-                winner = player
-        print("Final Outcome: ")
-        print(f"{curr_player.name} has gone bankrupt. {winner.name} wins.")
-        for player in players: 
-            if player.balance < 0:
-                print(f"{player.name} is bankrupt and finished on {board[player.curr_space].name}.")           
-            else:
-                print(f"{player.name} has a balance of {player.balance} and finished on {board[player.curr_space].name}.")
-        exit()
+        handle_end_game()
     
     curr_index = (curr_index + 1) % len(players)
