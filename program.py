88f54c04e0f5e7d8210dcad7f1d3320444525a8a
KrisPKreme <S3935358@student.rmit.edu.au>
2026-03-24 12:43:21 +1100

    implemented doubled rent for owning all properties of the same colour

--- program.py
+++ program.py
@@ -11,7 +11,7 @@ with open('board.json', 'r') as file:
         Space(
             space['name'],
             space.get('price'),
-            space.get('rent'),
+            space.get('colour'),
             space['type']
         )
         for space in json.load(file)
@@ -20,7 +20,16 @@ with open('board.json', 'r') as file:
 with open('rolls_1.json', 'r') as file:
     rolls_1 = json.load(file)
 
+# group spaces by colour in dictionary
+colour_groups = {}
+for space in board:
+    if space.colour is not None:
+        if space.colour not in colour_groups:
+            colour_groups[space.colour] = []
+        colour_groups[space.colour].append(space)
 
+
+# initialize four players
 Player1 = Player("Peter", starting_balance, starting_position)
 Player2 = Player("Billy", starting_balance, starting_position)
 Player3 = Player("Charlotte", starting_balance, starting_position)
@@ -35,7 +44,7 @@ for roll in rolls_1:
     curr_player = players[curr_index]
     space = curr_player.move(roll, board_size)
 
-    print(f"{curr_player.name} rolled {roll} and landed on {board[space].name} and has a balance of {curr_player.balance}")
+    # print(f"{curr_player.name} rolled {roll} and landed on {board[space].name} and has a balance of {curr_player.balance}")
 
     if board[space].name == "GO":
         curr_player.balance += 1
@@ -49,11 +58,16 @@ for roll in rolls_1:
         else:
             # player lands on owned property and must pay rent to owner
             if board[space].owner != curr_player.name:
-                curr_player.balance -= board[space].rent
+                # rent is doubled if all properties of the same colour are owned by the same player
+                if all(s.owner == board[space].owner for s in colour_groups[board[space].colour]):
+                    rent = board[space].price * 2
+                else:
+                    rent = board[space].price
+                curr_player.balance -= rent
                 for player in players:
                     if player.name == board[space].owner:
-                        player.balance += board[space].rent
-                print(f"{curr_player.name} paid {board[space].rent} rent to {board[space].owner} and has a balance of {curr_player.balance}")
+                        player.balance += rent
+                print(f"{curr_player.name} paid {rent} rent to {board[space].owner} and has a balance of {curr_player.balance}")
             # player lands on their own property and does nothing
             elif board[space].owner == curr_player.name:
                 print(f"{curr_player.name} landed on their own property {board[space].name} and has a balance of {curr_player.balance}")
